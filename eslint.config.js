cd186be026a404f17a65257cecef26bd792a364e
Oscar Lagatta <oscarlagatta@gmail.com>
2024-11-23 07:39:00 +0000

    Add shadcn library and Vitest configuration
    Implemented a new shared library "shadcn" including configuration files (README, eslint, tsconfig, babel). Integrated Vitest as the unit test runner for the project. Additionally, updated dependencies in the package-lock to support new components and utilities.

--- eslint.config.js
+++ eslint.config.js
@@ -17,8 +17,39 @@ module.exports = [
           allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
           depConstraints: [
             {
-              sourceTag: '*',
-              onlyDependOnLibsWithTags: ['*'],
+              sourceTag: 'type:app',
+              onlyDependOnLibsWithTags: ['type:ui', 'type:feature', 'type:utils', 'type:data'],
+            },
+            {
+              sourceTag: 'type:ui',
+              onlyDependOnLibsWithTags: [
+                'type:utils',
+                'type:models',
+                'scope:shared',
+              ],
+            },
+            {
+              sourceTag: 'type:feature',
+              onlyDependOnLibsWithTags: [
+                'type:ui',
+                'type:data',
+                'type:models',
+                'scope:shared',
+              ],
+            },
+
+            {
+              sourceTag: 'type:data',
+              onlyDependOnLibsWithTags: ['type:models', 'scope:shared'],
+            },
+            { sourceTag: 'type:models', onlyDependOnLibsWithTags: [] },
+            {
+              sourceTag: 'type:state',
+              onlyDependOnLibsWithTags: ['type:models'],
+            },
+            {
+              sourceTag: 'scope:app',
+              onlyDependOnLibsWithTags: ['type:ui', 'type:feature'],
             },
           ],
         },
