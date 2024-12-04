c1960a6e13dfbf3cac3ed2eb053f11e51b2a82a6
Oscar Lagatta <oscarlagatta@gmail.com>
2024-12-04 23:26:18 +0000

    Add new Landscape Services UI library
    Created a new UI library, `landscape-services-ui`, within the `landscape-hub` monorepo. This includes configuration files, initial components, and basic unit tests. Additionally, updated `tsconfig.base.json` to reference the new library and included necessary IDE configuration and gitignore files.

---
+++ libs/landscape-hub/landscape-services/shared/landscape-services-ui/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/landscape-services-ui';
---
+++ libs/landscape-hub/landscape-services/shared/landscape-services-ui/src/lib/landscape-services-ui.tsx
@@ -0,0 +1,14 @@
+import styled from 'tailwind';
+
+const StyledLandscapeServicesUi = styled.div`
+  color: pink;
+`;
+export function LandscapeServicesUi() {
+  return (
+    <StyledLandscapeServicesUi>
+      <h1>Welcome to LandscapeServicesUi!</h1>
+    </StyledLandscapeServicesUi>
+  );
+}
+
+export default LandscapeServicesUi;
