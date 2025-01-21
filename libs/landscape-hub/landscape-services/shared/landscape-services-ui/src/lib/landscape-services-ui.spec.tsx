ba7e8e69265f461fc1563ee10d4d9efc8b73d800
Moshe-Victor Solomon <moshevictor@yahoo.co.uk>
2025-01-21 18:51:29 +0000

    feat: ServiceListing- Added landscape-services/shared folders and landscape-services-ui lib.

---
+++ libs/landscape-hub/landscape-services/shared/landscape-services-ui/src/lib/landscape-services-ui.spec.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+//
+// import LandscapeServicesLayout from './landscape-services-layout';
+//
+// describe('LandscapeServicesUi', () => {
+//   it('should render successfully', () => {
+//     const { baseElement } = render(<LandscapeServicesLayout />);
+//     expect(baseElement).toBeTruthy();
+//   });
+// });
